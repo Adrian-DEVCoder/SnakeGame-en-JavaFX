5103d36aa309c45fde50fa50641e57d566017618
Dctr-LurS <adrianrg2004@gmail.com>
2023-11-27 22:44:19 +0100

    Commit de creacion del repositorio

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,128 @@
+package org.example;
+
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main extends Application {
+
+    @Override
+    public void start(Stage pStage) {
+        // Creación de un StackPane que servirá como contenedor principal
+        StackPane panelInicio = new StackPane();
+        panelInicio.setStyle("-fx-background-color: #26205C"); // Establece el color de fondo del StackPane
+
+        // Carga y configura la imagen del logotipo del juego
+        Image logo = new Image("file:logoSnake.gif");
+        ImageView contenedorImagen = new ImageView(logo);
+        contenedorImagen.setFitWidth(320); // Establece el ancho de la imagen
+        contenedorImagen.setPreserveRatio(true); // Conserva la proporción original de la imagen
+
+        //Creacion de un input para el nombre de usuario
+        TextField txfUsuario = new TextField();
+
+        //Estilo para el input del nombre de usuario
+        String estiloTextField = "-fx-border-width: 2px; -fx-border-color: #413C70; -fx-border-radius: 6px;-fx-background-color: #26205C; -fx-text-fill: white; -fx-font-size: 16px; -fx-padding: 10px 15px";
+        txfUsuario.setStyle(estiloTextField);
+        txfUsuario.setPromptText("Nombre de Usuario");
+        txfUsuario.setAlignment(Pos.CENTER);
+        txfUsuario.setFocusTraversable(false);
+        txfUsuario.setMaxWidth(200);
+
+        // Creación de botones "Jugar" y "Salir del Juego"
+        Button btnJugar = new Button("Jugar");
+        Button btnEstadisticas = new Button("Estadísticas");
+        Button btnConfiguracion = new Button("Configuración");
+        Button btnCerrar = new Button("Cerrar Juego");
+
+        // Estilo de los botones
+        String estiloBotones = "-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-font-size: 16px; -fx-padding: 10px 15px;";
+        btnJugar.setStyle(estiloBotones);
+        btnEstadisticas.setStyle(estiloBotones);
+        btnConfiguracion.setStyle(estiloBotones);
+        btnCerrar.setStyle(estiloBotones);
+        btnJugar.setPrefWidth(150);
+        btnEstadisticas.setPrefWidth(150);
+        btnConfiguracion.setPrefWidth(150);
+        btnCerrar.setPrefWidth(150);
+
+        // Acciones al hacer clic en los botones
+        btnJugar.setOnAction(event -> {
+            String usuario = txfUsuario.getText();
+            if (!usuario.isEmpty()) {
+                iniciarJuego(pStage, usuario);
+            } else {
+                txfUsuario.setStyle("-fx-border-color: red; -fx-background-color: #26205C; -fx-border-width: 2px; -fx-border-radius: 6px; -fx-text-fill: white; -fx-font-size: 16px; -fx-padding: 10px 15px");
+                // Crear una animación de translación horizontal para el campo de texto
+                TranslateTransition tt = new TranslateTransition(Duration.millis(100), txfUsuario);
+                tt.setByX(10); // Mover el campo de texto 10 píxeles a la derecha
+                tt.setCycleCount(6); // Repetir la animación 6 veces
+                tt.setAutoReverse(true); // Hacer que la animación se revierta automáticamente
+                // Reproducir la animación
+                tt.play();
+            }
+        });
+
+        btnEstadisticas.setOnAction(event -> iniciarEstadisticas(pStage));
+        btnConfiguracion.setOnAction(event -> iniciarConfiguracion(pStage));
+        btnCerrar.setOnAction(actionEvent -> pStage.close()); // Cierra la ventana al hacer clic en "Salir del Juego"
+
+        // Organizar los botones horizontalmente con un espacio de 20 entre ellos
+        HBox botones = new HBox(20, btnJugar, btnEstadisticas);
+        botones.setAlignment(Pos.CENTER); // Centrar los botones horizontalmente
+        botones.setPadding(new Insets(20,20,20,20)); // Añadir relleno alrededor de los botones
+        // Organizar los botones de configuracion y cerrar con un espacio de 20 entre ellos
+        HBox botonesInferior = new HBox(20,btnConfiguracion,btnCerrar);
+        botonesInferior.setAlignment(Pos.CENTER);
+        botonesInferior.setPadding(new Insets(5,20,5,20));
+        // Organizar el boton de cerrar juego horizontalmente debajo de los otros dos botones
+        VBox cuerpo = new VBox(4,txfUsuario,botones,botonesInferior);
+        cuerpo.setAlignment(Pos.CENTER);
+        botones.setPadding(new Insets(40,20,10,20));
+        // VBox para contener la imagen y los botones en la parte superior
+        VBox vboxSuperior = new VBox(20, contenedorImagen);
+        vboxSuperior.setAlignment(Pos.TOP_CENTER); // Centrar elementos verticalmente
+        vboxSuperior.setPadding(new Insets(60, 20, 40, 20)); // Añadir relleno alrededor del VBox superior
+
+        // Alineación del VBox superior en el StackPane
+        StackPane.setAlignment(vboxSuperior, Pos.CENTER);
+
+        // VBox para el layout principal que contiene la imagen y los botones
+        VBox layoutInicio = new VBox(5, vboxSuperior, cuerpo);
+        layoutInicio.setBackground(new Background(new BackgroundFill(Color.web("#26205C"), CornerRadii.EMPTY, Insets.EMPTY))); // Establecer el color de fondo del VBox principal
+
+        // Crear la escena y configurarla en el Stage
+        Scene escenaInicio = new Scene(layoutInicio, 500, 500); // Crear una escena con el VBox principal
+        pStage.setScene(escenaInicio); // Establecer la escena en el Stage
+        pStage.setTitle("Snake Game - Adrian Rodriguez Garcia"); // Establecer el título del Stage
+        pStage.show(); // Mostrar el Stage con la escena configurada
+    }
+
+    private void iniciarJuego(Stage pStage,String usuario) {
+        SnakeGame snakeGame = new SnakeGame(usuario);
+        snakeGame.start(pStage);
+    }
+
+    private void iniciarEstadisticas(Stage stage) {
+        Estadisticas estadisticas = new Estadisticas();
+        estadisticas.start(stage);
+    }
+
+    private void iniciarConfiguracion(Stage stage) {
+        Configuracion configuracion = new Configuracion();
+        configuracion.start(stage);
+    }
+}
---
+++ src/main/java/org/example/SnakeGame.java
@@ -0,0 +1,281 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SnakeGame extends Application {
+    private static final int tamanoSerpiente = 20;
+    private static final int ancho = 25;
+    private static final int alto = 25;
+    private Direccion direccion = Direccion.RIGHT;
+    private Canvas tablero;
+    private List<Position> serpiente = new ArrayList<>();
+    private boolean direccionCambiada;
+    private boolean derrota;
+    private Position comida;
+    private int marcador;
+    private Text textoMarcador;
+    private Text maximaPuntuacion;
+    private int puntuacion = 0;
+    private String nombreUsuario;
+
+    public SnakeGame(String nombreUsuario){
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage stage) {
+        // Inicializamos el tablero, textos de marcadores, escenas, listener para las teclas y movimientos
+        // ademas iniciamos el juego y el bucle principal de este.
+        tablero = new Canvas(ancho * tamanoSerpiente, alto * tamanoSerpiente);
+        // Text para el marcador con la puntuacion
+        textoMarcador = new Text();
+        textoMarcador.setX(10);
+        textoMarcador.setY(10);
+        textoMarcador.setFill(Color.WHITE);
+        textoMarcador.setFont(Font.font("verdana", FontPosture.REGULAR,15));
+        actualizarMarcador();
+        // Text para el marcador con la puntuacion mas alta
+        maximaPuntuacion = new Text();
+        maximaPuntuacion.setX(5);
+        maximaPuntuacion.setY(5);
+        maximaPuntuacion.setFill(Color.WHITE);
+        maximaPuntuacion.setFont(Font.font("verdana", FontPosture.REGULAR,15));
+        actualizarPuntuacionMaxima();
+        // Grid para colocar los texts
+        GridPane gridPane = new GridPane();
+        gridPane.addRow(0, textoMarcador);
+        gridPane.addColumn(1, maximaPuntuacion);
+        // Set de escena, paneles y contextos
+        GraphicsContext contextTablero = tablero.getGraphicsContext2D();
+        StackPane panel = new StackPane(tablero,gridPane);
+        Scene escena = new Scene(panel);
+        // Color de fondo ventana
+        panel.setStyle("-fx-background-color: #26205C");
+
+        // Listener para las flechas del teclado para realizar los movimientos
+        escena.setOnKeyPressed(keyEvent -> {
+            if(!direccionCambiada){
+                switch (keyEvent.getCode()){
+                    case UP:
+                        if(direccion != Direccion.DOWN)
+                            direccion = Direccion.UP;
+                        break;
+                    case DOWN:
+                        if(direccion != Direccion.UP)
+                            direccion = Direccion.DOWN;
+                        break;
+                    case LEFT:
+                        if(direccion != Direccion.RIGHT)
+                            direccion = Direccion.LEFT;
+                        break;
+                    case RIGHT:
+                        if(direccion != Direccion.LEFT)
+                            direccion = Direccion.RIGHT;
+                        break;
+                }
+                direccionCambiada = true;
+            }
+        });
+
+        // Titulo de la ventana y set de la escena a mostrar
+        stage.setScene(escena);
+        stage.setTitle("Snake Game - Adrián Rodríguez García");
+        stage.show();
+
+        // Iniciamos el juego
+        iniciarJuego();
+        iniciarBuclePrincipal(contextTablero,stage);
+    }
+
+    // Metodo para actualizar el marcador de la puntuacion maxima
+    private void actualizarPuntuacionMaxima() {
+        maximaPuntuacion.setText("\t\tMáxima puntuacion: "+puntuacion);
+    }
+
+    // Metodo para actualizar el marcador
+    private void actualizarMarcador() {
+        textoMarcador.setText("\tMarcador: "+marcador);
+    }
+
+    // Metodo para iniciar el bucle principal del juego
+    private void iniciarBuclePrincipal(GraphicsContext gc,Stage stage) {
+        new AnimationTimer() {
+
+            long ultimaActualizacion = 0;
+
+            // Metodo para la actualizacion del juego
+            @Override
+            public void handle(long now) {
+                if(now - ultimaActualizacion >= 100_000_000){
+                    ultimaActualizacion = now;
+                    if(!derrota){
+                        direccionCambiada = false;
+                        try {
+                            actualizar(stage);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        dibujar(gc);
+                    } else {
+                        stop();
+                    }
+                }
+            }
+        }.start();
+
+    }
+
+    // Metodo para iniciar el juego
+    private void iniciarJuego(){
+        marcador = 0;
+        actualizarMarcador();
+        serpiente.clear();
+        serpiente.add(new Position(ancho/2,alto/2));
+        direccion = Direccion.RIGHT;
+        spawnComida();
+        derrota = false;
+        direccionCambiada = false;
+    }
+
+    // Metodo para spawnear comida
+    private void spawnComida(){
+        int x = (int) (Math.random() * ancho);
+        int y = (int) (Math.random() * alto);
+        comida = new Position(x,y);
+    }
+
+    // Metodo para actualizar dentro del juego (movimientos, tamaño, derrota)
+    private void actualizar(Stage stage) throws Exception {
+        Position cabeza = serpiente.get(0).copiar();
+        switch (direccion){
+            case UP:
+                cabeza.y--;
+                break;
+            case DOWN:
+                cabeza.y++;
+                break;
+            case LEFT:
+                cabeza.x--;
+                break;
+            case RIGHT:
+                cabeza.x++;
+                break;
+        }
+        // Condicion para cuando la cabeza pase por la comida que aumente el tamaño de la serpiente
+        if(cabeza.equals(comida)){
+            serpiente.add(0, cabeza);
+            marcador++;
+            if(marcador > puntuacion){
+                puntuacion = marcador;
+                actualizarPuntuacionMaxima();
+            }
+            actualizarMarcador();
+            spawnComida();
+        } else {
+            // Condicion para cuando la serpiente choca con las colisiones
+            if(cabeza.x < 0 || cabeza.x >= ancho || cabeza.y < 0 || cabeza.y >= alto){
+                derrota = true;
+                iniciarDerrota(stage,marcador,derrota);
+                return;
+            }
+
+            // Condicion para cuando la serpiente choca con su propio cuerpo
+            for(Position cuerpo : serpiente){
+                if(cabeza.equals(cuerpo)){
+                    derrota = true;
+                    iniciarDerrota(stage,marcador,derrota);
+                    return;
+                }
+            }
+
+            serpiente.remove(serpiente.size() - 1);
+            serpiente.add(0, cabeza);
+
+        }
+    }
+
+    // Metodo para dibujar la comida y la serpiente
+    private void dibujar(GraphicsContext gc) {
+        gc.clearRect(0,0,ancho * tamanoSerpiente, alto * tamanoSerpiente);
+        gc.setFill(Color.RED);
+        gc.fillOval(comida.x * tamanoSerpiente, comida.y * tamanoSerpiente, tamanoSerpiente, tamanoSerpiente);
+        gc.setFill(Color.valueOf("#24694D"));
+        for(Position cuerpo : serpiente){
+            gc.fillOval(cuerpo.x * tamanoSerpiente, cuerpo.y * tamanoSerpiente, tamanoSerpiente, tamanoSerpiente);
+        }
+    }
+
+    private void iniciarDerrota(Stage stage, int puntos, boolean derrotado) throws Exception {
+        Derrota derrota1 = new Derrota(nombreUsuario);
+        derrota1.start(stage);
+        if (derrotado) {
+            // Crear un objeto RegistroPuntuacion con el nombre de usuario y la puntuación
+            RegistroPuntuacion registro = new RegistroPuntuacion(nombreUsuario, puntos);
+
+            // Obtener la lista de puntuaciones existente o una lista vacía si el archivo no existe
+            List<RegistroPuntuacion> puntuaciones = obtenerPuntuacionesJSON();
+
+            // Agregar el nuevo registro a la lista
+            puntuaciones.add(registro);
+
+            // Ordenar la lista por puntajes en orden descendente
+            puntuaciones.sort(Collections.reverseOrder());
+
+            // Limitar la lista a los 10 primeros elementos si es más grande
+            if (puntuaciones.size() > 10) {
+                puntuaciones = puntuaciones.subList(0, 10);
+            }
+
+            // Escribir la lista actualizada al archivo JSON
+            guardarPuntuacionesJSON(puntuaciones);
+        }
+    }
+    // Metodo para obtener las puntuaciones dentro del archivo JSON
+    private List<RegistroPuntuacion> obtenerPuntuacionesJSON() {
+        Gson gson = new Gson();
+        File archivo = new File("marcadores.json");
+
+        if (archivo.exists()) {
+            try (FileReader reader = new FileReader(archivo)) {
+                Type tipoLista = new TypeToken<List<RegistroPuntuacion>>(){}.getType();
+                return gson.fromJson(reader, tipoLista);
+            } catch (IOException e) {
+                System.out.println("Error al leer el archivo de puntuaciones.");
+                return new ArrayList<>();
+            }
+        } else {
+            return new ArrayList<>();
+        }
+    }
+    // Metodo para guardar las puntuaciones dentro del archivo JSON
+    private void guardarPuntuacionesJSON(List<RegistroPuntuacion> puntuaciones){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try(FileWriter fw = new FileWriter("marcadores.json")){
+            gson.toJson(puntuaciones, fw);
+        } catch (IOException e) {
+            System.out.println("No se ha podido crear el archivo.");
+        }
+    }
+
+}
